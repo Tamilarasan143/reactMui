51402a670fd6ad49ab23f6c57295ea09ed2930c5
Tamilarasan Elumalai <tamilarasan@jsansoftwares.com>
2023-08-03 15:30:07 +0530

    addUserTextfiled

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <Grid container component={Paper} height={"100vh"} sx={{backgroundColor:"red"}}display={"flex"} justifyContent="center" alignItems={"center"}>
      <Box component={Paper} width={"600px"} p={2} flexDirection="column" height={"80%"} gap={2} display={"flex"} justifyContent="center" alignItems={"center"} >
       <Typography variant='h5'>Hello Everyone</Typography>
+      <TextField label="username"fullWidth margin='normal'/>
       <TextField label="email" fullWidth margin='normal'/>
       <TextField label="password"fullWidth margin='normal'/>
       <FormControlLabel
